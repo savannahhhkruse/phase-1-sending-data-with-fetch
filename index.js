592f26af1985740ec61611655bdd476fde178ddf
Savannah <skruse101@gmail.com>
2023-05-27 16:30:39 -0700

    sending data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+function submitData(name, email) {
+    const formData = {
+      name: name,
+      email: email
+    };
+  
+    const configObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+    };
+  
+    return fetch("http://localhost:3000/users", configObj)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(object) {
+        const newId = object.id;
+        document.body.innerHTML += newId;
+      })
+      .catch(function(error) {
+        document.body.innerHTML += error.message;
+      });
+  }
